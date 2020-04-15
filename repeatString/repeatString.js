746a445ce7d2aebe04cf61ca596c0f50879cfce7
Aaron Contreras <aaronlcaq@gmail.com>
2020-04-15 18:47:22 -0500

    Add solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, timesToRepeat) {
+  if (timesToRepeat < 0) {
+    return 'ERROR';
+  }
 
+  let repeatedString = '';
+  for (let i = 0; i < timesToRepeat; i++) {
+    repeatedString += word;
+  }
+
+  return repeatedString;
 }
 
 module.exports = repeatString
