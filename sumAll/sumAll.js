99b5cfe6a21d4c35ad1a35c50508c7e8c226bd7b
Aaron Contreras <aaronlcaq@gmail.com>
2020-04-16 14:51:39 -0500

    Add solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0 || +start !== start || +end !== end) {
+    return 'ERROR';
+  }
 
+  let range = [start, end];
+  if (start > end) {
+    range.reverse();
+  }
+
+  let sum = 0;
+  for (let i = range[0]; i <= range[1]; i++) {
+    sum += i;
+  }
+  
+  return sum;
 }
 
 module.exports = sumAll
