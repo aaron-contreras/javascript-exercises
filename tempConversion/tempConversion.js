55fa7b34a5e127803e526e69672b3b004ec62072
Aaron Contreras <aaronlcaq@gmail.com>
2020-04-16 16:29:05 -0500

    Add solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (farenheit) {
+  const celsius = (farenheit - 32) * 5 / 9;
+  const temperature = +celsius.toFixed(1);
+  return temperature;
 }
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  const farenheit = celsius * 9 / 5 + 32;
+  const temperature = +farenheit.toFixed(1);
+  return temperature;
 }
 
 module.exports = {
